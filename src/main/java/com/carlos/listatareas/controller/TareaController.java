0deba1785b7542a327ab19a5fb4c749b2a444ff7
munozcar20 <carlosmz243@gmail.com>
2022-07-14 11:30:01 -0500

    Controller is completed

--- src/main/java/com/carlos/listatareas/controller/TareaController.java
+++ src/main/java/com/carlos/listatareas/controller/TareaController.java
@@ -2,17 +2,20 @@ package com.carlos.listatareas.controller;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.carlos.listatareas.dto.TareaDto;
 import com.carlos.listatareas.services.ITareaService;
 
 @RestController
 public class TareaController {
     
     Object response;
-
-
 
     @Autowired
     ITareaService tareaService;
@@ -23,5 +26,21 @@ public class TareaController {
         return response; 
     }
 
+    @GetMapping(path = "tareasporid/{id}")
+    public Object buscarPorId(@PathVariable("id") long id){
+        response = tareaService.buscarTarea(id);
+        return response;
+    }
 
+    @PostMapping(path = "nuevo")
+    public Object guardar(@RequestBody TareaDto tareaDto){
+        response = tareaService.guardarTarea(tareaDto);
+        return response;
+    }
+
+    @DeleteMapping(path = "eliminar/{id}")
+    public void eliminar(@PathVariable("id") long id){
+        tareaService.eliminarTarea(id);
+        
+    }
 }
